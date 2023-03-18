50e7958ce434c3732809e3567f8d870c746ce1ae
Derek Cochran <derek@derekcochran.dev>
2023-03-18 10:14:19 -0500

    add options and no games error handing

--- steamutils.py
+++ steamutils.py
@@ -7,24 +7,52 @@ from steam import steamid
 from steam.webapi import WebAPI
 from typing import Union
 
-SETTINGS_DIR = user_data_dir('steamutils', 'Skylark95')
-SETTINGS_FILE = os.path.join(SETTINGS_DIR, 'settings.json')
-API_KEY = 'api_key'
+class Settings:
+    settings_dir = user_data_dir('steamutils', 'Skylark95')
+    settings_file = os.path.join(settings_dir, 'settings.json')
 
-def read_settings(key: str = None) -> Union[dict, str]:
-    settings = {}
-    if os.path.isfile(SETTINGS_FILE):
-        with open(SETTINGS_FILE, 'r') as f:
-            settings = json.load(f)
-    if key == None:
-        return settings
-    else:
-        return settings.get(key)
+    def __init__(self):
+        self.data = {}
+        if os.path.isfile(self.settings_file):
+            with open(self.settings_file, 'r') as f:
+                self.data = json.load(f)
+    
+    def get(self, key):
+        return self.data.get(key)
+    
+    def set(self, key, value) -> str:
+        self.data[key] = value
+        os.makedirs(self.settings_dir, exist_ok=True)
+        with open(self.settings_file, 'w') as f:
+            json.dump(self.data, f)
 
-def write_settings(settings: dict) -> None:
-    os.makedirs(SETTINGS_DIR, exist_ok=True)
-    with open(SETTINGS_FILE, 'w') as f:
-        json.dump(settings, f)
+class SteamID:
+    def __init__(self, click: click, user: str):
+        self.click = click
+        self.steam_id = steamid.from_url(f"https://steamcommunity.com/id/{user}")
+        if self.steam_id is None:
+            click.echo(f"Could not find Steam user: {user}")
+            sys.exit(1)
+        self.steam64 = self.steam_id.as_64
+
+    def echo(self):
+        self.click.echo(f"AccountID: {self.steam_id.account_id}")
+        self.click.echo(f"SteamID: {self.steam_id.id}")
+        self.click.echo(f"Steam2 ID: {self.steam_id.as_steam2}")
+        self.click.echo(f"Steam3 ID: {self.steam_id.as_steam3}")
+        self.click.echo(f"Steam64: {self.steam_id.as_64}")
+
+class SteamAPI:
+    def __init__(self, click: click):
+        settings = Settings()
+        api_key = settings.get('api_key')
+        if (api_key == None):
+            click.echo(f"API Key not set.\n\nSee steamutils apikey --help")
+            sys.exit(1)
+        self.web_api = WebAPI(api_key)
+
+    def call(self, method_path, **kwargs):
+        return self.web_api.call(method_path, **kwargs)
 
 @click.command()
 @click.argument('key')
@@ -32,42 +60,46 @@ def apikey(key):
     """Set the Steam API Key.
     
     KEY is the api key to set."""
-    settings = read_settings()
-    settings[API_KEY] = key
-    write_settings(settings)
+    settings = Settings()
+    settings.set('api_key', key)
 
 @click.command()
 @click.argument('user')
-def games(user):
+@click.option('--csv/--json', default=True, help="Output format. Default is --csv")
+@click.option('--appid/--no-appid', default=False, help="Include app id in the response. Default is --no-appid")
+@click.option('--free/--no-free', default=False, help="Include free games in the output. Default is --no-free")
+def games(user, csv, appid, free):
     """Lookup owned steam games.
     
     USER is the name of the steam user"""
-    api_key = read_settings(API_KEY)
-    if api_key == None:
-        click.echo(f"API Key not set.\n\nSee steamutils apikey --help")
-        sys.exit(1)
-
-    steam_id = steamid.from_url(f"https://steamcommunity.com/id/{user}")
-    if steam_id is None:
-        click.echo(f"Could not find Steam user: {username}")
-        sys.exit(1)
-
+    steam_id = SteamID(click, user)
+    api = SteamAPI(click)
     options = {
-        'steamid': steam_id.as_64,
+        'steamid': steam_id.steam64,
         'include_appinfo': True, 
-        'include_played_free_games': False,
+        'include_played_free_games': free,
         'appids_filter': None,
-        'include_free_sub': False,
+        'include_free_sub': free,
         'language': 'en',
         'include_extended_appinfo': False
     }
-
-    api = WebAPI(api_key)
     result = api.call('IPlayerService.GetOwnedGames', **options)
+    if not hasattr(result['response'], 'games'):
+        click.echo('User has no games or profile is set to private.')
+        sys.exit(1)
     games_list = result['response']['games']
     games_list.sort(key=lambda game: game['name'])
+    output_list = []
     for game in games_list:
-        click.echo(game['name'])
+       output = { 'game': game['name'] } 
+       output_list.append(output)
+       if appid:
+           output['appid'] = str(game['appid'])
+    if csv:
+        for output in output_list:
+            click.echo(','.join(output.values()))
+    else:
+        click.echo(json.dumps(output_list))
 
 @click.command()
 @click.argument('username')
@@ -75,16 +107,8 @@ def user(username):
     """Lookup SteamID by username.
     
     USERNAME is the name of the steam user to lookup."""
-    steam_id = steamid.from_url(f"https://steamcommunity.com/id/{username}")
-    if steam_id is None:
-        click.echo(f"Could not find Steam user: {username}")
-        sys.exit(1)
-
-    click.echo(f"AccountID: {steam_id.account_id}")
-    click.echo(f"SteamID: {steam_id.id}")
-    click.echo(f"Steam2 ID: {steam_id.as_steam2}")
-    click.echo(f"Steam3 ID: {steam_id.as_steam3}")
-    click.echo(f"Steam64: {steam_id.as_64}")
+    steam_id = SteamID(click, username)
+    steam_id.echo()
     
 @click.group()
 def cli():
